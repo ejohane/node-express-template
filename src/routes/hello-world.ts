f27028ea565b68a1ddd507bfa9f314d132d01758
Erik Johansson <ejohane@gmail.com>
2023-01-14 22:40:57 -0600

    add meta

--- src/routes/hello-world.ts
+++ src/routes/hello-world.ts
@@ -1,10 +1,15 @@
-import { defaultEndpointsFactory } from "express-zod-api";
+import { defaultEndpointsFactory, withMeta } from "express-zod-api";
 import { getHelloWorldHandler } from "../handlers/hello-world-handlers";
 import { HelloRequestSchema, HelloResponseSchema } from "../dto/hello-dto";
 
 export const helloWorldEndpoint = defaultEndpointsFactory.build({
   method: "get",
-  input: HelloRequestSchema,
-  output: HelloResponseSchema,
+  input: withMeta(HelloRequestSchema).example({
+    name: "Erik",
+  }),
+  output: withMeta(HelloResponseSchema).example({
+    greetings: "Greetings Erik!",
+  }),
   handler: getHelloWorldHandler,
+  description: "Hello World",
 });
